96ab05bd12aad16ecf99dfd981ad718b855762d5
mando157 <mando.1572005@gmail.com>
2026-03-19 12:48:41 +0200

    animation making

--- script.js
+++ script.js
@@ -5,26 +5,16 @@ form.addEventListener('submit', function (e) {
     alert("Form submitted!");
 });
 
-
-
+const card = document.querySelector('.login-card');
 const showSignup = document.getElementById('showSignup');
 const showLogin = document.getElementById('showLogin');
-const loginContainer = document.querySelector('.login-container');
-const signupContainer = document.querySelector('.signup-container');
 
 showSignup.addEventListener('click', function (e) {
     e.preventDefault();
-    loginContainer.style.display = 'none';
-    signupContainer.style.display = 'block';
-    loginContainer.style.opacity = '0';
-    signupContainer.style.opacity = '1';
+    card.classList.add('active');
 });
 
 showLogin.addEventListener('click', function (e) {
     e.preventDefault();
-    signupContainer.style.display = 'none';
-    loginContainer.style.display = 'block';
-    signupContainer.style.opacity = '0';
-    loginContainer.style.opacity = '1';
-
+    card.classList.remove('active');
 });
