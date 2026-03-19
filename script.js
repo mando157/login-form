a5cca2794ee9010fb513fae6b1cd29a5c884d793
mando157 <mando.1572005@gmail.com>
2026-03-19 14:50:16 +0200

    Validation & POP Up Done

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const form = document.getElementById('loginForm');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert("Form submitted!");
-});
+//* Animation of the cards
 
 const card = document.querySelector('.login-card');
 const showSignup = document.getElementById('showSignup');
@@ -17,4 +12,76 @@ showSignup.addEventListener('click', function (e) {
 showLogin.addEventListener('click', function (e) {
     e.preventDefault();
     card.classList.remove('active');
+});
+
+//* POP UP Animation 
+
+
+const loginForm = document.getElementById('loginForm');
+const signUpForm = document.getElementById('signupForm');
+const loginPopUP = document.getElementById('login-popup');
+const signUpPopUP = document.getElementById('signup-popup');
+const okButtonL = document.getElementById('ok-button-l');
+const okButtonS = document.getElementById('ok-button-s');
+
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    setTimeout(loginPopUP.style.transform = 'translate(-50% , 0)', 2000);
+
+});
+
+okButtonL.addEventListener("click", function (e) {
+    setTimeout(loginPopUP.style.transform = 'translate(-50% , -100%)', 2000);
+})
+
+signUpForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    setTimeout(signUpPopUP.style.transform = 'translate(-50% , 0)', 2000);
+
+});
+
+okButtonS.addEventListener("click", function (e) {
+    setTimeout(signUpPopUP.style.transform = 'translate(-50% , -100%)', 2000);
+})
+
+
+//* Validation
+
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    if (!email.includes("@")) {
+        alert("Enter a Valid email!");
+        return;
+    }
+
+    if (password === "" || email === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters!");
+        setTimeout(loginPopUP.style.transform = 'translate(-50% , -100%)', 2000);
+        return;
+    }
+})
+signupForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+
+    if (name === "" || email === "" || password === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters!");
+        setTimeout(signUpPopUP.style.transform = 'translate(-50% , -100%)', 2000);
+        return;
+    }
 });
